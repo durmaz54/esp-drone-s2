538ca5034376fcd82f58fe2027276d4eceab799e
durmaz54 <abdulsametdurmaz54@gmail.com>
2024-05-25 21:36:53 +0300

    wifi ap mode and udp successful

---
+++ main/main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_check.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+
+#include "esp_wifi.h"
+#include "esp_private/wifi.h"
+
+#include "tinyusb.h"
+#include "tinyusb_net.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#define UDP_IP "192.168.4.2"
+#define UDP_PORT 1234
+
+static const char *TAG = "USB_NCM";
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_task(){
+{
+    
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+    
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+    
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .ssid_len = strlen(CONFIG_ESP_WIFI_SSID),
+            .channel = 4,
+            .password = CONFIG_ESP_WIFI_PASSWORD,
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    if (strlen(CONFIG_ESP_WIFI_PASSWORD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID");
+}
+
+    
+} 
+
+void imu_task(){
+    
+    char tx_buffer[128];
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(UDP_IP);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(UDP_PORT);
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+
+    while (1)
+    {
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1 saniye bekleme
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        continue;
+    }
+    ESP_LOGI(TAG, "Socket created, sending to %s:%d", UDP_IP, UDP_PORT);
+
+    while (1) {
+        snprintf(tx_buffer, sizeof(tx_buffer), "Message from ESP32");
+        int err = sendto(sock, tx_buffer, strlen(tx_buffer), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Message sent");
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS); // 2 saniye bekleme
+    }
+    }
+    
+   
+
+    if (sock != -1) {
+        ESP_LOGE(TAG, "Shutting down socket and restarting...");
+        shutdown(sock, 0);
+        close(sock);
+    }
+    vTaskDelete(NULL);
+    
+}
+
+void app_main(void)
+{
+
+    /* Initialize NVS — it is used to store PHY calibration data */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    wifi_task();
+    xTaskCreate(imu_task, "imu_task", 4096, NULL, 5, NULL);
+    
+}
